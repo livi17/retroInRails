090318103247c931009b537870a9af62e39beb56
Michael LaRiviere <michael.lariviere@aceroinc.ca>
2015-09-21 10:41:55 -0400

    added faker 1.4.2 to gemfile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
 	before_action :correct_user,   only: [:edit, :update]
 
 	def index
+		#variable containinbg all the sites users.
 		@users = User.all
   	end
 
